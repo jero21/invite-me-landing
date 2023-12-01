7c4ba9343f1ba1631122289570b4b0eab1020be6
Jeremias Mora Cuevas <jeremias.morac@gmail.com>
2023-11-30 23:01:14 -0300

    página dahiana y gustavo

--- invitaciones/dahiana-gustavo/countdown.js
+++ invitaciones/dahiana-gustavo/countdown.js
@@ -1,8 +1,8 @@
 simplyCountdown('#cuenta', {
 	year: 2024, // required
 	month: 2, // required
-	day: 24, // required
-	hours: 16, // Default is 0 [0-23] integer
+	day: 16, // required
+	hours: 19, // Default is 0 [0-23] integer
 	minutes: 30, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
