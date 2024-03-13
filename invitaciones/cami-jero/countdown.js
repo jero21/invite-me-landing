cbc58ef8bd2728c35faadf5960db22e0065471ba
Jeremias Mora Cuevas <jeremias.morac@gmail.com>
2024-03-12 22:48:57 -0300

    Reemplazo completo del proyecto con la nueva versión

--- invitaciones/cami-jero/countdown.js
+++ invitaciones/cami-jero/countdown.js
@@ -1,8 +1,8 @@
 simplyCountdown('#cuenta', {
 	year: 2025, // required
 	month: 2, // required
-	day: 10, // required
-	hours: 18, // Default is 0 [0-23] integer
+	day: 24, // required
+	hours: 16, // Default is 0 [0-23] integer
 	minutes: 30, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
