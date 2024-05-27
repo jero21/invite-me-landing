7bc32e29b18d236aa5039a8bbfde930de6c15d6a
Jeremias Mora Cuevas <jeremias.morac@gmail.com>
2024-05-26 22:39:56 -0400

    cambio horario

--- invitaciones/carolina-jonathan/countdown.js
+++ invitaciones/carolina-jonathan/countdown.js
@@ -2,8 +2,8 @@ simplyCountdown('#cuenta', {
 	year: 2024, // required
 	month: 9, // required
 	day: 28, // required
-	hours: 18, // Default is 0 [0-23] integer
-	minutes: 0, // Default is 0 [0-59] integer
+	hours: 17, // Default is 0 [0-23] integer
+	minutes: 30, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
 		days: 'Día',
