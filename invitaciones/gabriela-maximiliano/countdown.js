caed21e947accddaca6833de2ae593435cd77afe
Jeremias Mora Cuevas <jeremias.morac@gmail.com>
2024-07-04 16:11:29 -0400

    gabriela y maximiliano

--- invitaciones/gabriela-maximiliano/countdown.js
+++ invitaciones/gabriela-maximiliano/countdown.js
@@ -1,7 +1,7 @@
 simplyCountdown('#cuenta', {
 	year: 2024, // required
 	month: 11, // required
-	day: 14, // required
+	day: 30, // required
 	hours: 17, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
