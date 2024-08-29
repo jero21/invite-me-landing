8abfda848cba1184eecb57da189ed54cad3c6c43
Jeremias Mora Cuevas <jeremias.morac@gmail.com>
2024-08-29 00:52:01 -0400

    cambios maria-raul

--- invitaciones/maria-raul/countdown.js
+++ invitaciones/maria-raul/countdown.js
@@ -1,7 +1,7 @@
 simplyCountdown('#cuenta', {
 	year: 2024, // required
-	month: 12, // required
-	day: 7, // required
+	month: 11, // required
+	day: 23, // required
 	hours: 17, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
